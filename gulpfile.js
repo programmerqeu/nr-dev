870bda3116eb2c32235849a3c904df8420dae8e3
André Lademann <andre@programmerq.eu>
2014-06-11 22:31:11 +0200

    Use tab to indent in files

--- gulpfile.js
+++ gulpfile.js
@@ -13,41 +13,41 @@ var paths = {
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
+gulp.task('sass', function (done) {
   gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+	  .pipe(sass())
+	  .pipe(gulp.dest('./www/css/'))
+	  .pipe(minifyCss({
+		keepSpecialComments: 0
+	  }))
+	  .pipe(rename({extname: '.min.css'}))
+	  .pipe(gulp.dest('./www/css/'))
+	  .on('end', done);
 });
 
-gulp.task('guard', function(done) {
+gulp.task('guard', function (done) {
 });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.sass, ['sass', 'guard']);
+gulp.task('watch', function () {
+  gulp.watch(paths.sass, ['sass', 'guard']);
 });
 
-gulp.task('install', ['git-check'], function() {
+gulp.task('install', ['git-check'], function () {
   return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+	  .on('log', function (data) {
+		gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+	  });
 });
 
-gulp.task('git-check', function(done) {
+gulp.task('git-check', function (done) {
   if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
+	console.log(
+		'  ' + gutil.colors.red('Git is not installed.'),
+		'\n  Git, the version control system, is required to download Ionic.',
+		'\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+		'\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+	);
+	process.exit(1);
   }
   done();
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,38 @@
 angular.module('nrdev.controllers', [])
 
-        .controller('AppCtrl', function($scope) {
-        })
+	.controller('AppCtrl', function ($scope) {
+	})
 
-        .controller('HomeCtrl', function($scope) {
-            var page = {title: 'Developers home'};
-            $scope.page = page;
+	.controller('HomeCtrl', function ($scope) {
+	  var page = {title: 'Developers home'};
+	  $scope.page = page;
 
-            // start google analytics tracking
-            if (typeof analytics !== 'undefined') {
-                analytics.trackView(page.title);
-            }
-        })
+	  // start google analytics tracking
+	  if (typeof analytics !== 'undefined') {
+		analytics.trackView(page.title);
+	  }
+	})
 
-        .controller('ImpressCtrl', function($scope) {
-            var page = {title: 'Impress'};
-            $scope.page = page;
+	.controller('ImpressCtrl', function ($scope) {
+	  var page = {title: 'Impress'};
+	  $scope.page = page;
 
-            // start google analytics tracking
-            if (typeof analytics !== 'undefined') {
-                analytics.trackView(page.title);
-            }
-        })
+	  // start google analytics tracking
+	  if (typeof analytics !== 'undefined') {
+		analytics.trackView(page.title);
+	  }
+	})
 
-        .controller('PlaylistsCtrl', function($scope) {
-            $scope.playlists = [
-                {title: 'Reggae', id: 1},
-                {title: 'Chill', id: 2},
-                {title: 'Dubstep', id: 3},
-                {title: 'Indie', id: 4},
-                {title: 'Rap', id: 5},
-                {title: 'Cowbell', id: 6}
-            ];
-        })
+	.controller('PlaylistsCtrl', function ($scope) {
+	  $scope.playlists = [
+		{title: 'Reggae', id: 1},
+		{title: 'Chill', id: 2},
+		{title: 'Dubstep', id: 3},
+		{title: 'Indie', id: 4},
+		{title: 'Rap', id: 5},
+		{title: 'Cowbell', id: 6}
+	  ];
+	})
 
-        .controller('PlaylistCtrl', function($scope, $stateParams) {
-        })
+	.controller('PlaylistCtrl', function ($scope, $stateParams) {
+	})
