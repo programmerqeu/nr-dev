a6e21730c25358e85b509e37a0d514020e84c943
André Lademann <andre@programmerq.eu>
2014-08-07 23:21:23 +0200

    #8 Reduce gulp dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,19 @@
 /**
  * NRdev
  *
- * @category   Deployment
- * @sucategory Taskmanger
- * @package    NRdev
- * @author     André Lademann <andre.lademann@netresearch.de>
- * @license    https://netresearch.de/license
- * @version    0.2.0
+ * @category   	Deployment
+ * @subcategory Taskmanger
+ * @package    	NRdev
+ * @author     	André Lademann <andre.lademann@netresearch.de>
+ * @license    	https://netresearch.de/license
+ * @version    	0.3.0
  */
 
 var bower = require('bower');
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var dalek = require('gulp-dalek');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var gutil = require('gulp-util');
 var sh = require('shelljs');
 
-var paths = {
-	sass: ['./scss/**/*.scss']
-};
 
-
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function (done) {
-	gulp.src('./scss/main.scss')
-		.pipe(sass())
-		.pipe(gulp.dest('./www/css/'))
-		.pipe(minifyCss({
-			keepSpecialComments: 0
-		}))
-		.pipe(rename({extname: '.min.css'}))
-		.pipe(gulp.dest('./www/css/'))
-		.on('end', done);
-});
-
-gulp.task('test', function () {
-	'use strict';
-	return gulp.src(['tests/integration/*.js'])
-		.pipe(dalek({
-			browser: ['phantomjs'],
-			reporter: ['html', 'console']
-		}));
-});
-
-gulp.task('guard', function (done) {
-});
-
-gulp.task('watch', function () {
-	gulp.watch(paths.sass, ['sass', 'guard']);
-});
 
 gulp.task('install', ['git-check'], function () {
 	return bower.commands.install()
