05d2d696dd3b3b99c8f47d90f05c492dfb7bab38
vergissberlin@googlemail.com <vergissberlin@googlemail.com>
2014-09-10 20:33:09 +0200

    #1 Add time-grunt to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,8 @@
 
 'use strict';
 module.exports = function (grunt) {
-
+	// show elapsed time at the end
+	require('time-grunt')(grunt);
 	// load all grunt tasks
 	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
